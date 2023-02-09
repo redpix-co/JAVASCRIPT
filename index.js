5e47edda0b41342fe122a80b13bcd148566d501b
redpix-co <72983943+redpix-co@users.noreply.github.com>
2023-02-09 15:45:30 +0530

    promise, method and callback

--- index.js
+++ index.js
@@ -1,14 +1,51 @@
-console.log('ss');
+// ---------------------------------------------------------
+// BASIC PROMISE
+
 // const suc = new Promise((resolve, reject) => {
 //   setTimeout(() => {
 //     resolve('success1');
 //   }, 1000);
 // });
 
-const fai = new Promise((resolve, reject) => {
+// const fai = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve('Failur');
+//   }, 1000);
+// });
+
+// fai.then(console.log).catch(console.log);
+
+// ---------------------------------------------------------
+// BASIC PROMISE METHOD
+// all, allSettled, race
+const comm = new Promise((resolve, reject) => {
   setTimeout(() => {
-    resolve('Failur');
+    resolve('success1');
   }, 1000);
 });
 
-fai.then(console.log).catch(console.log);
+const all = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject('Failur');
+    // change resolve (all method)
+    // change reject (common)
+  }, 100);
+  // reduce time (race method)
+});
+
+Promise.race([comm, all]).then(console.log).catch(console.log);
+
+// ---------------------------------------------------------
+// BASIC CALLBACK FUNCTION
+
+// const normal = (val, secondFun) => {
+//   return secondFun(val + 5);
+// }
+
+// const secondFun = (val) => {
+//   return val - 25;
+// }
+
+// console.log(normal(10, secondFun))
+
+// ---------------------------------------------------------
